af5f8e3908be68df4d20e28b438067d7c316a5ad
Shovan Chowdhury <shovanchowdhury@Shovans-MacBook-Air.local>
2024-03-09 14:35:22 +0530

    second

--- dbms.py
+++ dbms.py
@@ -11,10 +11,17 @@ mydb = m.connect(
 )
 
 mycursor = mydb.cursor()
-reg_name = input("Enter Registration number: ")
+
+reg_no = input("Enter Registration number: ")
 first_name = input("Enter first name: ")
 last_name = input("Enter last name: ")
 gender = input("Enter patient's gender (Male/Female/Other): ")
 dob = input("Enter date of birth: ")
 contact_number = input("Enter patient's contact number: ")
+
+insert_query = "INSERT INTO Patients (reg_no, first_name, last_name, dob, gender, contact_number) VALUES (%s, %s, %s, %s, %s, %s)"
+mycursor.execute(insert_query, (reg_no, first_name, last_name, dob, gender, contact_number))
+
+mydb.commit()
+
 
